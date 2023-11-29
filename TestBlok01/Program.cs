6ade5cf2b16f42dd6af9bafcf900dff1604ad51b
Vit <mayllby@gmail.com>
2023-11-29 17:45:09 +0300

    Добавил решение

--- TestBlok01/Program.cs
+++ TestBlok01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача: 
+// Написать программу, которая из имеющегося массива строк 
+// формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма.
+
+string text = 
+            "Hello, a2, world, :-)" +
+			"1234, 156, -2, Ok, computer science," +
+			"Russia, Denmark, Kazan";
+
+Console.Write("["+ text +"] ->"); 
+
+string phrase = text;
+string[] words = phrase.Split(' ', ',');
+
+foreach (var word in words)
+if (word.Length <= 3) Console.Write(word +' ');
