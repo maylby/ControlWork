32961547b5c2cdd52df32066d507d6a43d6caf93
Vit <mayllby@gmail.com>
2023-11-30 00:51:31 +0300

    Отформатировал текст ('txt')

--- TestBlok01/Program.cs
+++ TestBlok01/Program.cs
@@ -23,10 +23,9 @@ if (word.Length <= 3) Console.Write(word +' ');
 
 // Вариант 2 
 
-string txt = 
-            "Hello, 2, world, :-)"+
-			"1234, 1567, -2, computer science"+
-			"Russia, Denmark, Kazan";
+string txt = "Hello, 2, world, :-)"
+			+ "1234, 1567, -2, computer science"
+			+ "Russia, Denmark, Kazan";
 
 string ChoiceWords(string txt)
 {
